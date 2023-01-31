d343c86346fb89539b847d3e16fcd35a62fe9393
fullzer4 <gabrielpelizzaro@gmail.com>
2023-01-31 11:18:07 -0300

    new

--- Beggin/loops/src/main.rs
+++ Beggin/loops/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut i: i32 = 1;
+    let something: i32 = loop {
+        i *= 2;
+        if i > 100 {
+            break i;
+        }
+    };
+    assert_eq!(something, 128);
+
+    let mut counter: i32 = 0;
+
+    while counter < 10 {
+        println!("hello");
+        counter = counter + 1;
+    }
+
+    for item in 0..5 {
+        println!("{}", item*2);
+    }
 }
